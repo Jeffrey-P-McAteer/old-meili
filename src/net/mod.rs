33a77db094576c3c88a48c67e61716ec9ab01ea2
Jeffrey McAteer <jeffrey.p.mcateer@gmail.com>
2020-07-28 07:15:06 -0400

    initial upnp experimental stuff; the goal is to have a large set of nat-traversal options available so users can talk to eachother and discover other users without a centralized server.

--- src/net/mod.rs
+++ src/net/mod.rs
@@ -1,5 +1,6 @@
 
 use igd;
+use get_if_addrs;
 
 use std::thread;
 use std::sync::Arc;
@@ -101,10 +102,75 @@ fn attempt_upnp_setup(args: &Vec<String>, config: &Config, global: &Global) -> R
     ..Default::default()
   };
   let gw = igd::search_gateway(igd_opts)?;
+  //println!("gw={:?}", &gw);
 
-  let local_addr = SocketAddrV4::new(Ipv4Addr::new(127, 0, 0, 1), config.upnp_local_port as u16);
-  gw.add_port(igd::PortMappingProtocol::UDP, 80, local_addr, 60, "meili port mapping")?;
+  let mut lan_ip_a: Ipv4Addr = Ipv4Addr::BROADCAST;
+  // We pick the first 
+  match get_if_addrs::get_if_addrs() {
+    Ok(if_addrs) => {
+      for addr in if_addrs {
+        if addr.is_loopback() {
+          continue;
+        }
+        // igd only takes SocketAddrV4 so we cannot use ipv6 for this :(
+        if let IpAddr::V4(addr) = addr.ip() {
+          lan_ip_a = addr;
+          break;
+        }
+      }
+    }
+    Err(e) => {
+      println!("e={:?}", e);
+      // Not exactly the most appropriate error but we'll use it
+      return Err(igd::Error::GetExternalIpError( igd::GetExternalIpError::ActionNotAuthorized ));
+    }
+  }
+  println!("lan_ip_a={:?}", &lan_ip_a);
 
+  let local_addr = SocketAddrV4::new(lan_ip_a.clone(), config.upnp_local_port as u16);
+  let mut external_port = config.upnp_pref_public_port as u16;
+
+  // First search existing ports + exit if one has our lan_ip_a and config.upnp_local_port and config.upnp_pref_public_port
+  let mut i = 0;
+  loop {
+    match gw.get_generic_port_mapping_entry(i) {
+      Ok(entry) => {
+        println!(
+          "i={} external_port={} internal_client={} port_mapping_description={} lease_duration={}",
+          i, entry.external_port, entry.internal_client, entry.port_mapping_description, entry.lease_duration
+        );
+        let is_match = entry.port_mapping_description.contains("meili") &&
+                       entry.internal_client == format!("{}", lan_ip_a) &&
+                       entry.internal_port == config.upnp_local_port as u16;
+        if is_match {
+          // return b/c we already have an entry; TODO Write external_port to global?
+          println!("Already have requested UPNP port mapping on public port :{}", entry.external_port);
+          return Ok(());
+        }
+      }
+      Err(igd::GetGenericPortMappingEntryError::RequestError(re))  => {
+        println!("re={:?}", re);
+        continue;
+      }
+      Err(e)  => {
+        break;
+      }
+    }
+    if i > 256 {
+      break; // Sanity check
+    }
+    i += 1;
+  }
+
+  let lease_duration_s = 300;
+  if let Err(e) = gw.add_port(igd::PortMappingProtocol::UDP, external_port, local_addr, lease_duration_s, "meili port mapping") {
+    println!("upnp e={:?}", e);
+    // Attempt w/ random public port
+    external_port = gw.add_any_port(igd::PortMappingProtocol::UDP, local_addr, lease_duration_s, "meili port mapping")?;
+  }
+  
+  // Write external_port to global? Not sure what the most useful action is here.
+  println!("Added requested UPNP port mapping on public port :{}", external_port);
 
   Ok(())
 }
